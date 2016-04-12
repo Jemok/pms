aec200083ce09c86ab47b051af96084a14a55c6c
renn <renncomp58@gmail.com>
2016-04-12 20:57:26 +0300

    renamed createproject migration

--- database/migrations/2016_04_12_173955_create_projects_table_migration.php
+++ database/migrations/2016_04_12_173955_create_projects_table_migration.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProjetsTableMigration extends Migration
+class CreateProjectsTableMigration extends Migration
 {
     /**
      * Run the migrations.
