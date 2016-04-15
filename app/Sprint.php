a33a01b96a537239a78325fd1ae4bac1eed89ea1
James Mwangi <karokijames40@gmail.com>
2016-04-16 02:49:41 +0300

    Updated models

--- app/Sprint.php
+++ app/Sprint.php
@@ -37,4 +37,31 @@ class Sprint extends Model
 
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * A sprint has many tasks
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function task(){
+
+        return $this->hasMany(Task::class);
+    }
+
+    /**
+     * A sprint has many sprint tasks
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function sprint_task(){
+
+        return $this->hasMany(Sprint_task::class);
+    }
+
+    /**
+     * A Sprint has many users
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function sprint_user(){
+
+        return $this->hasMany(Sprint_user::class);
+    }
 }
--- app/Team.php
+++ app/Team.php
@@ -25,4 +25,22 @@ class Team extends Model
 
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * A Team has many projects
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function project(){
+
+        return $this->hasMany(Project::class);
+    }
+
+    /**
+     * A team has many team users
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function team_user(){
+
+        return $this->hasMany(Team_user::class);
+    }
 }
