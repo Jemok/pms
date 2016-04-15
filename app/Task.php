c80a733196d482696dc69240de9f1a7b404e8040
James Mwangi <karokijames40@gmail.com>
2016-04-16 01:50:57 +0300

    Add models for the various entities

---
+++ app/Task.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * Fields that can be mass assigned
+     * @var array
+     */
+    protected $fillable = [
+
+        'task_name',
+        'task_description',
+        'deliverable',
+        'task_status',
+        'started_at',
+        'ended_at'
+
+    ];
+
+    /**
+     * A Sprint belongs to a task
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function sprint(){
+
+        return $this->belongsTo(Sprint::class);
+
+    }
+
+    /**
+     * A task belongs to a user
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(){
+
+        return $this->belongsTo(User::class);
+
+    }
+}
---
+++ app/Team_user.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User;
+
+class Team_user extends Model
+{
+    /**
+     * The fields that might be mass assigned
+     * @var array
+     */
+    protected $fillable = [
+
+        'team_id',
+        'user_id'
+
+    ];
+
+    /**
+     * A team Team user belongs to a team
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function team(){
+
+        return $this->belongsTo(Team::class);
+    }
+
+    /**
+     * A team user is a user
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(){
+
+        return $this->belongsTo(User::class);
+    }
+}
