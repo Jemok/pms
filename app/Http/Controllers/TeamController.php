b49887535271b908b9b34f896276df51ea48868a
Ngizeh <edngize@gmail.com>
2016-04-16 04:23:57 +0300

    create TeamController and TeamReaquests

---
+++ app/Http/Controllers/TeamController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use App\Team;
+use App\Http\Requests\CreateTeamRequest;
+class TeamController extends Controller
+{
+
+    public function store(CreateTeamRequest $createTeamRequest)
+    {
+        Team::create($createTeamRequest->all());
+
+        Session::flash('flash_message', 'Team was created successfully');
+        return redirect()->back();
+    }
+
+
+
+}
