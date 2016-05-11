b087c87ca2d465deff195d800105b7a97fbf04db
renn <renncomp58@gmail.com>
2016-05-11 21:39:23 +0300

    working on projects views

--- resources/views/teams/user_teams.blade.php
+++ resources/views/teams/user_teams.blade.php
@@ -10,26 +10,26 @@
                     <div class="panel-body">
                         {{--heading--}}
                         <div class="row">
-                            <div class="col-md-3">
+                            <div class="col-md-3 col-sm-3">
                                 <h5><strong>Team name</strong></h5>
                             </div>
-                            <div class="col-md-4 col-md-offset-1">
+                            <div class="col-md-4 col-md-offset-1 col-sm-4 col-sm-offset-1">
                                 <h5><strong>Short description</strong></h5>
                             </div>
-                            <div class="col-md-2">
+                            <div class="col-md-2 col-sm-2">
                                 <h5><strong>User level</strong></h5>
                             </div>
                         </div>{{--end of heading--}}
 
                         {{--display data--}}
                         <div class="row">
-                            <div class="col-md-3">
-                                <p><strong>Team 1</strong></p>
+                            <div class="col-md-3 col-sm-2">
+                                <p>Team 1</p>
                             </div>
-                            <div class="col-md-4 col-md-offset-1">
-                                <p><strong>blah blah blah</strong></p>
+                            <div class="col-md-4 col-md-offset-1 col-sm-4 col-sm-offset-1">
+                                <p>blah blah blah</p>
                             </div>
-                            <div class="col-md-2">
+                            <div class="col-md-2 col-sm-2">
                                 <p><button type="button" class="btn btn-info btn-sm">level</button></p>
                             </div>
                         </div>{{--end of display--}}
