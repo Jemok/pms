9e8691bc94720f7c0cc0088e35dc1b34a1790ff0
James Mwangi <karokijames40@gmail.com>
2016-05-12 03:49:47 +0300

    Adds only admin to edit team in team details

--- resources/views/teams/team_details.blade.php
+++ resources/views/teams/team_details.blade.php
@@ -48,9 +48,11 @@
             <div class="col-md-2">
                 <a href="{{ url('profile/userProfile') }}">{{$team->admin->user->name}}</a>
             </div>
+            @if($team->admin->user_id == \Auth::user()->id)
             <div class="col-md-2">
                 <p><a href="{{ url('teams/editTeam/'.$team->id) }}" class="btn btn-default">edit this team</a></p>
             </div>
+            @endif
         </div>
 
         <div class="row">
